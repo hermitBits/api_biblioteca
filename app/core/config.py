a038a798502e35d056d5ccf276aa8396abd119bb
Mateus Camargo <mateuscamargo0@outlook.com.br>
2022-05-28 22:18:01 -0300

    Settings Default

--- app/core/config.py
+++ app/core/config.py
@@ -8,8 +8,8 @@ class Settings:
     DATABASE_USER: str = getenv('DATABASE_USER')
     DATABASE_PASSWORD: str = getenv('DATABASE_PASSWORD')
     DATABASE_NAME: str = getenv('DATABASE_NAME')
-    DATABASE_SERVER: str = getenv('DATABASE_SERVER')
-    DATABASE_PORT: str = getenv('DATABASE_PORT')
+    DATABASE_SERVER: str = getenv('DATABASE_SERVER', "localhost")
+    DATABASE_PORT: str = getenv('DATABASE_PORT', 3306)
     DATABASE_URL: str = f"mysql+mysqlconnector://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_SERVER}:{DATABASE_PORT}/{DATABASE_NAME}"
     
     
